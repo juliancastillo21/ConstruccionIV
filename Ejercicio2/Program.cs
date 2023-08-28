efa401eb445371a15076e99d66fed67f33755d93
juliancastillo21 <oscarjuliancastillo@gmail.com>
2023-08-27 19:38:04 -0500

    se agrego el archivo Program del ejercicio2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("TABLAS DE MULTIPLICAR");
+
+Console.Write("Ingrese el inicio del rango: ");
+int rangoInicio = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el fin del rango: ");
+int rangoFin = int.Parse(Console.ReadLine());
+Console.Write("**************\n");
+
+for (int numero = rangoInicio; numero <= rangoFin; numero++) {
+        Console.WriteLine($"Tabla de multiplicar del {numero}:");
+    for(int i=1; i<=10;i++){
+        Console.WriteLine(numero + " X " + i + " = " + numero*i);
+    }
+}
